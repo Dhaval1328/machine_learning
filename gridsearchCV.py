92d063b98dd4bc4cd8f2c81c59d9a658181af1d3
Dhaval Parmar <ddhaval2655@gmail.com>
2026-06-30 11:57:37 +0530

    Update gridsearchCV.py

--- gridsearchCV.py
+++ gridsearchCV.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import GridSearchCV
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as  plt
 import seaborn as sns
 
 # Load dataset

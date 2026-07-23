2c458b8081dc1c47c84a2768ff9bb74ef3278bbc
Dhaval Parmar <ddhaval2655@gmail.com>
2026-07-23 09:42:34 +0530

    Add files via upload

---
+++ 23-07/simpleregression_profit.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+data=pd.read_csv("D:/4066/23-07/linearregressiondataset.csv")
+
+x=data.iloc[:,0:1]
+y=data.iloc[:,1]
+
+print(x)
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Creating and fitting the model
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+# Print the coefficients and intercept
+print('Coefficients:', model.coef_)
+print('Intercept:', model.intercept_)
+
+# FIXED: Passed as a DataFrame matching feature name to remove UserWarning
+y_pred=model.predict(pd.DataFrame([[20.27]], columns=x.columns))
+print('profit for the population of 20.27 lakh is:',y_pred)
+
+y_pred = model.predict(X_test)
+
+# FIXED: Changed 'salary' to 'profit'
+print("predicted profit is:",y_pred)
+
+# Model evaluation 
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
